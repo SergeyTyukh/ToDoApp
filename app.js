1c765b642d2c4c80269a5cb438a95c0ccba17517
Sergey Tyukh <115223206+SergeyTyukh@users.noreply.github.com>
2022-12-30 13:12:56 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-const ButtonElement = document.getElementById("removeBtn");
-const TaskElement = document.getElementById("list");
-
-
-ButtonElement.addEventListener("click", () =>{
-    const MyDiv = document.getElementById("list");
-    const removeItem = MyDiv.parentNode;
-    TaskElement.innerHTML = MyDiv.value;
-    removeItem.removeChild(MyDiv)
-})
-
+document.getElementById('removeBtn').onclick = function () {
+    var i, elements = document.getElementsByClassName('taskForm');
+    for (i = elements.length; i--;) {         
+      elements[i].parentNode.removeChild(elements[i]);
+    }
+}         
