ff451781687a30edfc9f79281ce79102e5e160c5
Sergey Tyukh <115223206+SergeyTyukh@users.noreply.github.com>
2022-12-30 14:49:48 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 let delform = document.getElementById("removeBtn");
+const ButtonElement = document.getElementById("addBtn");
+const InputElement = document.getElementById("typing");
+const ListElement = document.getElementById("list");
+
+
 
 delform.addEventListener("click", () => {
-  let i,
-    elements = document.getElementsByClassName("taskForm");
-  for (i = elements.length; i--; ) {
-    elements[i].parentNode.removeChild(elements[i]);
+  let i,elements = document.getElementsByClassName("taskForm");
+    for (i = elements.length; i--; ) {
+        elements[i].parentNode.removeChild(elements[i]);
   }
 });
+
+
+
+ButtonElement.addEventListener('click', () => {
+    let taskForm = document.createElement('div')
+    taskForm.classList.add('taskForm')
+    taskForm.innerHTML = InputElement.value;
+    ListElement.append(taskForm)
+})
+
