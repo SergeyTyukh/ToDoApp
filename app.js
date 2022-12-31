ae12b92ebe2e1a35695149192516acb91059ac6c
Sergey Tyukh <115223206+SergeyTyukh@users.noreply.github.com>
2022-12-31 13:17:48 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,33 +3,43 @@ const ButtonElement = document.getElementById("addBtn");
 const InputElement = document.getElementById("typing");
 const ListElement = document.getElementById("list");
 const FormElement = document.getElementById("general");
+const RemoveElement = document.querySelector(".taskForm");
 
-
+//remove all tasks
 delform.addEventListener("click", () => {
   let i,elements = document.getElementsByClassName("taskForm");
     for (i = elements.length; i--; ) {
         elements[i].parentNode.removeChild(elements[i]);
-  }
-});
+      }
+    });
+  
+  
+  //add task
+  FormElement.addEventListener("submit", (event) => {
+    let taskForm = document.createElement("div");
+    let taskaddBtn = document.createElement("button");
+    taskaddBtn.id = "checkBtn";
+    taskaddBtn.className = "check";
+    let taskPoint = document.createElement("div");
+    taskPoint.className = "text";
+    let taskdelBtn = document.createElement("button");
+    taskdelBtn.id = "delBtn";
+    taskdelBtn.className = "delete";
+    
+    taskForm.classList.add("taskForm");
+    taskPoint.innerHTML = InputElement.value;
+    taskForm.appendChild(taskaddBtn);
+    taskForm.appendChild(taskPoint);
+    taskForm.appendChild(taskdelBtn);
+    ListElement.appendChild(taskForm);
+    event.preventDefault();
+    
+    taskdelBtn.addEventListener('click', () => {
+      ListElement.removeChild(taskForm);
+  });
+  });
+  
+  
 
 
-
-FormElement.addEventListener("submit", (event) => {
-  let taskForm = document.createElement("div");
-  let taskaddBtn = document.createElement("button");
-   taskaddBtn.id = "checkBtn";
-   taskaddBtn.className = "check";
-  let taskPoint = document.createElement("div");
-   taskPoint.className = "text";
-  let taskdelBtn = document.createElement("button");
-   taskdelBtn.id = "delBtn";
-   taskdelBtn.className = "delete";
-
-  taskForm.classList.add("taskForm");
-  taskPoint.innerHTML = InputElement.value;
-  taskForm.appendChild(taskaddBtn);
-  taskForm.appendChild(taskPoint);
-  taskForm.appendChild(taskdelBtn);
-  ListElement.appendChild(taskForm);
-  event.preventDefault();
-});
+     
